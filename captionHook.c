3f1dc9867e410f98e748ce00a5f1cdf487827a11
Hoang Hao Quang <32516654+HQhalo@users.noreply.github.com>
2019-11-06 14:06:15 +0700

    Update captionHook.c

--- captionHook.c
+++ captionHook.c
@@ -11,41 +11,45 @@
 #include <linux/moduleparam.h>
 #include <linux/unistd.h>
 #include <asm/cacheflush.h>
+
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("D0hnuts");
-/*MY sys_call_table address*/
-//ffffffff81601680
+
 void **system_call_table_addr;
-/*my custom syscall that takes process name*/
-asmlinkage int (*custom_syscall) (char* name);
-/*hook*/
-asmlinkage int captain_hook(char* play_here) {
- /*do whatever here (print "HAHAHA", reverse their string, etc)
- But for now we will just print to the dmesg log*/
- printk(KERN_INFO "Pname Syscall:HOOK! HOOK! HOOK! HOOK!...ROOOFFIIOO!");
- return custom_syscall(play_here);
+
+
+asmlinkage int (*custom_syscall) (const char __user *pathname);
+
+
+asmlinkage int my_sys_open( const char __user *pathname) {
+    if(strcmp(current->comm , "test")==0){
+        char path[100];
+        copy_from_user(path,pathname,strnlen_user(pathname,100));
+        printk(KERN_INFO "HOOK!");
+        printk("THREAD NAME = %s  FILE = %s \n\n", current->comm,pathname);
+
+    }
+    return  custom_syscall(pathname);
+
 }
 
 static int __init entry_point(void){
- write_cr0 (read_cr0 () & (~ 0x10000));
- printk(KERN_INFO "Captain Hook loaded successfully..\n");
- /*MY sys_call_table address*/
- system_call_table_addr = (void*)0xffffffff81601680;
- /* Replace custom syscall with the correct system call name (write,open,etc) to hook*/
- custom_syscall = system_call_table_addr[__NR_pname];
- 
- system_call_table_addr[__NR_pname] = captain_hook;
- write_cr0 (read_cr0 () | 0x10000);
- return 0;
+    write_cr0 (read_cr0 () & (~ 0x10000));
+    printk(KERN_INFO "Captain Hook loaded successfully..\n");
+    system_call_table_addr = (void*)0xffffffff82000280;
+    custom_syscall = system_call_table_addr[__NR_openat];
+    
+    system_call_table_addr[__NR_openat] = my_sys_open;
+    write_cr0 (read_cr0 () | 0x10000);
+    return 0;
 }
-static int __exit exit_point(void){
+static void __exit exit_point(void){
  write_cr0 (read_cr0 () & (~ 0x10000));
  printk(KERN_INFO "Unloaded Captain Hook successfully\n");
- /*Restore original system call */
- system_call_table_addr[__NR_pname] = custom_syscall;
+ system_call_table_addr[__NR_openat] = custom_syscall;
  
  write_cr0 (read_cr0 () | 0x10000);
- return 0;
+
 }
 module_init(entry_point);
 module_exit(exit_point);
